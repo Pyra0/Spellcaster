ff5139e9266f0b07ffd69df38dd875af9271592b
BuildTools <unconfigured@null.spigotmc.org>
2022-06-26 01:59:49 +0100

    Added BaseCommand class

---
+++ src/main/java/me/pyra0/spellcaster/commands/BaseCommand.java
@@ -0,0 +1,11 @@
+package me.pyra0.spellcaster.commands;
+
+import org.bukkit.command.Command;
+
+public abstract class BaseCommand extends Command {
+
+    public BaseCommand(String name) {
+        super(name);
+    }
+
+}
--- src/main/java/me/pyra0/spellcaster/utils/Util.java
+++ src/main/java/me/pyra0/spellcaster/utils/Util.java
@@ -3,5 +3,5 @@ package me.pyra0.spellcaster.utils;
 public final class Util {
     private Util() {}
 
-    
+
 }
